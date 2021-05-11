aea47721714f44382cfe3d8d47c02bd36f21de9a
matias <matcardillo91@gmail.com>
2021-05-11 14:10:05 -0300

    sdfsdf

--- database/config/config.js
+++ database/config/config.js
@@ -1,12 +1,12 @@
 module.exports= {
   "development": {
     "username": "root",
-    "password": "",
-    "database": "e-commerce-prog2",
-    "host": "127.0.0.1",
+    "password": "root",
+    "database": "tpfinal",
+   
     "dialect": "mysql",
     "operatorsAliases": false,
-    "port" : 3306
+    "port" : "3307"
   },
   "test": {
     "username": "root",
@@ -18,7 +18,7 @@ module.exports= {
   },
   "production": {
     "username": "root",
-    "password": null,
+    "password": null,   
     "database": "database_production",
     "host": "127.0.0.1",
     "dialect": "mysql",
